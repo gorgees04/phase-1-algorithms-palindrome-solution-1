84d45683683e92ba8cdb1920cd6041f2886a4eb6
Gorgees Odisho <gorgees041@gmail.com>
2023-10-10 21:01:47 +0300

    ASSIGNMENET DONE

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // check the if the revese word is same as word using reverse() function
+  if (reverse(word) === word) {
+    return true
+  } else {
+    return false
+  }
+}
+
+const reverse = (word) => {
+  // split the word to make array of chars and reverse the array and join them together
+  return word.split("").reverse().join("")
 }
 
 /* 
-  Add your pseudocode here
+  Pseudocode
+  - make a function "reverse()" to reverse the word:
+    - split the word to make an array of chars
+    - use reverse() method to revorse array 
+    - use join() to join the char and make backword word 
+
+  - chech if word and reverse word using reverse function
+    if they are same return true
+    if not the same return false 
 */
 
 /*
-  Add written explanation of your solution here
+  explanation of solution:
+  - simply made a reverse function to reverse the word 
+  - compare word with the reverse word using the function I made 
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +41,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+  
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("anna"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("sam"));
 }
 
 module.exports = isPalindrome;
